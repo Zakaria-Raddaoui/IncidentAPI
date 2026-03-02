30d0483cb6972e05366703cbd4ebd66ed28f8100
Zakariya Raddaoui <zakariyadexterr8@gmail.com>
2026-03-02 10:30:42 +0100

    Configured database connection

---
+++ Models/IncidentsDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace IncidentAPI.Models
+{
+    public class IncidentsDbContext : DbContext
+    {
+        public IncidentsDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected IncidentsDbContext()
+        {
+        }
+        public virtual DbSet<Incident> Incidents { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using IncidentAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,7 +9,13 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(); 
+builder.Services.AddSwaggerGen();
+
+// Automatic db connection since startup
+builder.Services.AddDbContext<IncidentsDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.
+    GetConnectionString("IncidentsConnection")));
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
